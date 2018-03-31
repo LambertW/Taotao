e6d154421826946f1c88bef52cd3f9c36d22f753
LambertWu <26795806@qq.com>
2018-03-31 23:26:52 +0800

    add portal web project

---
+++ taotao-portal-web/src/main/java/com/lambertwu/portal/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.lambertwu.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 首页展示
+ * @author wgq19
+ *
+ */
+@Controller
+public class IndexController {
+
+	
+	@RequestMapping("/index")
+	public String showIndex() {
+		return "index";
+	}
+}
